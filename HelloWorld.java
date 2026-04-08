ed65aa2f90ca34a1f1ba2dfe282cb0aa2a80ba5d
kdavenpo229 <kdavenpo98@gmail.com>
2026-04-08 03:27:50 +0000

    Add HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("My name is Katherine.");
+        System.out.println("I am taking Java 1 and this is my first program!");
+    }
+}
